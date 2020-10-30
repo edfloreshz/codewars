71ddede731e179e77a62e2c31f1495d0c7a48ffb
Eduardo Flores <edfloreshz@gmail.com>
2020-10-30 12:37:27 -0700

    simplified sort_binary method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod solutions;
 
 fn main() {
-  let array = vec![0,1,4,2,3,4,5,16,6,7,8];
-  assert_eq!(vec![0,1,2,4,4,8,16,3,5,6,7], solutions::binary_representation::sort_binary(array));
+  let mut array = vec![0,1,4,2,3,4,5,16,6,7,8];
+  assert_eq!(vec![0,1,2,4,4,8,16,3,5,6,7], *solutions::binary_representation::sort_binary(&mut array));
 }
--- src/solutions/binary_representation.rs
+++ src/solutions/binary_representation.rs
@@ -1,11 +1,5 @@
-pub fn sort_binary(input: Vec<i32>) -> Vec<i32> {
-	let mut input = input;
+pub fn sort_binary(input: &mut Vec<i32>) -> &mut Vec<i32> {
 	input.sort();
-	let mut map = input.iter().map(|num| {
-		let ones = format!("{:b}", num).chars().filter(|bin| *bin == '1').collect::<String>();
-		(num, ones.len())
-	}).collect::<Vec<(&i32, usize)>>();
-	map.sort_by(|a, b| a.1.cmp(&b.1));
-	let map: Vec<i32> = map.iter().map(|x| *x.0).collect::<Vec<i32>>();
-	map
+	input.sort_by(|a, b| a.count_ones().cmp(&b.count_ones()));
+	input
 }
